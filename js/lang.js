da23afde1739de016a84a5cb077d7e8df3ca3b87
Sprax2013 <developer@sprax2013.de>
2019-04-19 17:27:09 +0200

    Added noscript + status-modifier + error message
    + Code cleanup (style.css)

--- js/lang.js
+++ js/lang.js
@@ -3,13 +3,17 @@ const lang = function () {
         en: {
             combo: 'Combo',
             points: 'Points',
-            thousandsSeparator: ','
+            thousandsSeparator: ',',
+
+            error: 'Installation instructions: <a href="https://github.com/Sprax2013/Modern-Beat-Saber-Overlay#installation-obs">https://github.com/Sprax2013/Modern-Beat-Saber-Overlay#installation-obs</a><br><hr><br>Looks like you are visiting the page outside of OBS (Your browser blocked the request to Beat Saber).<br><br>If you use the page outside of OBS and see this message, I ask you to report the error at <a href="https://github.com/Sprax2013/Modern-Beat-Saber-Overlay/issues">https://github.com/Sprax2013/Modern-Beat-Saber-Overlay/issues</a>.'
         },
 
         de: {
             combo: 'Combo',
             points: 'Punkte',
-            thousandsSeparator: '.'
+            thousandsSeparator: '.',
+
+            error: 'Installationsanweisungen: <a href="https://github.com/Sprax2013/Modern-Beat-Saber-Overlay#installation-obs">https://github.com/Sprax2013/Modern-Beat-Saber-Overlay#installation-obs</a><br><hr><br>Anscheinend besuchen Sie die Seite außerhalb von OBS (Ihr Browser hat die Anfrage an Beat Saber blockiert).<br><br>Wenn Sie die Seite außerhalb von OBS verwenden und diese Meldung sehen, bitte ich Sie, den Fehler unter <a href="https://github.com/Sprax2013/Modern-Beat-Saber-Overlay/issues">https://github.com/Sprax2013/Modern-Beat-Saber-Overlay/issues</a> zu melden.'
         }
     };
 
--- js/manager.js
+++ js/manager.js
@@ -2,25 +2,52 @@ function connect() {
     var IP = query.get('ip') || 'localhost';
     var PORT = query.get('port') || 6557;
 
-    var socket = new WebSocket(`ws://${IP}:${PORT}/socket`);
+    let statusElem = document.getElementById('status');
+    statusElem.classList.remove('hidden');
+    statusElem.innerHTML = 'Connecting...';
 
-    socket.addEventListener('open', () => {
-        console.log('WebSocket opened');
-    });
+    try {
+        var socket = new WebSocket(`ws://${IP}:${PORT}/socket`);
 
-    socket.addEventListener('message', (message) => {
-        var data = JSON.parse(message.data);
-        var event = events[data.event];
+        socket.addEventListener('open', () => {
+            console.log('WebSocket opened');
 
-        if (event) {
-            event(data.status, data.time);
-        }
-    });
+            statusElem.classList.add('hidden');
+            statusElem.innerText = 'Connected!';
+        });
 
-    socket.addEventListener('close', () => {
-        console.log('Failed to connect to Beat Saber, retrying in 3 seconds');
-        setTimeout(connect, 3000);
-    });
+        socket.addEventListener('message', (message) => {
+            var data = JSON.parse(message.data);
+            var event = events[data.event];
+
+            if (event) {
+                event(data.status, data.time);
+            }
+        });
+
+        socket.addEventListener('close', () => {
+            console.log('Failed to connect to Beat Saber, retrying in 3 seconds');
+
+            statusElem.classList.remove('hidden');
+            statusElem.innerText = 'Failed to connect to Beat Saber, retrying in 3 seconds';
+
+            setTimeout(() => {
+                statusElem.innerText = 'Failed to connect to Beat Saber, retrying in 2 seconds';
+
+                setTimeout(() => {
+                    statusElem.innerText = 'Failed to connect to Beat Saber, retrying in 1 second';
+
+                    setTimeout(connect, 1000);
+                }, 1000);
+            }, 1000);
+        });
+    } catch (err) {
+        console.error(err);
+
+        let errElem = document.getElementById('error');
+        errElem.innerHTML = lang.error;
+        errElem.classList.remove('hidden');
+    }
 }
 
 connect();
